15df52fa9fc72c3a8d43091a3083fdcb3eed6c7b
Armour <497052684@qq.com>
2015-11-27 12:05:20 -0800

    add asteroid hazards

---
+++ Assets/Scripts/RandomRotator.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RandomRotator : MonoBehaviour {
+
+	public float tumble;
+
+	private Rigidbody rb;
+	
+	public void Start() {
+		rb = GetComponent<Rigidbody>();
+		rb.angularVelocity = Random.insideUnitSphere * tumble;
+	}
+}
