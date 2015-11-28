b7565e98bfbcd79fbb9ae382e94802627128ff53
Armour <497052684@qq.com>
2015-11-27 21:36:21 -0800

    add GameController

--- Assets/Scripts/DestoryByContact.cs
+++ Assets/Scripts/DestoryByContact.cs
@@ -3,9 +3,18 @@ using System.Collections;
 
 public class DestoryByContact : MonoBehaviour {
 
+	public GameObject explosion;
+	public GameObject playerExplosion;
+
 	void OnTriggerEnter(Collider other) {
 		if (other.tag == "Boundary")
 			return;
+
+		Instantiate(explosion, transform.position, transform.rotation);
+
+		if (other.tag == "Player") 
+			Instantiate(playerExplosion, other.transform.position, other.transform.rotation);
+
 		Destroy(other.gameObject);
 		Destroy(gameObject);
 	}
